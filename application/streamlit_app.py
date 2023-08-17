30b7feb563d7d15dd0447467fcfe340ad365fc69
Timo <timo.bohnstedt@accenture.com>
2023-08-17 14:34:50 +0200

    Fix chat such that chat history is used. Refactored Code structure and created archiv.

--- application/streamlit_app.py
+++ application/streamlit_app.py
@@ -1,88 +1,60 @@
-# Import Streamlit
-
+import openai
 import streamlit as st
-from azure_services import create_response
-import streamlit as st
-import numpy as np
-import plotly.figure_factory as ff
-import azure_services as chatbot
-from datetime import time
+import json
 
-# Here, import your application functions
-# For example, you might have a file `azure_services.py` in the application folder:
-# from application.azure_services import some_azure_function
-
-# Placeholder function to simulate application processing
-# Replace this with actual calls to Azure Cognitive Services in your application code
+def load_credentials(filename="./application/credentials.json"):
+    with open(filename, 'r') as file:
+        credentials = json.load(file)
+    return credentials
 
 
-# Streamlit App
+def load_config(filename="./application/config.json"):
+    with open(filename, 'r') as file:
+        config = json.load(file)
+    return config
 
+st.title("ChatGPT-like clone")
 
+openai.api_key = "ec6b8771460d416aa289f4e1748851be"
+credentials = load_credentials()
+config = load_config()
 
-def main():
-    """
-    The main Streamlit application. This function defines the layout and flow of the app.
-    """
-    st.title('LAIwand Generative AI Demo')
+openai.api_type = credentials['api_type']
+openai.api_base = credentials['api_base']
+openai.api_version = credentials['api_version']
+openai.api_key = credentials['api_key']
 
-    st.write('Welcome to the LAIwand Generative AI Demo!')
-    st.write('This app is a prototype developed for the Accenture Corporate Hackathon.')
+model_engine = config['model_engine']
+temperature = config['temperature']
 
-    # Initialize chat history
-    if "messages" not in st.session_state:
-        st.session_state.messages = []
+if "openai_model" not in st.session_state:
+    st.session_state["openai_model"] = "gpt-3.5-turbo"
 
-    # Display chat messages from history on app rerun
-    for message in st.session_state.messages:
-        with st.chat_message(message["role"]):
-            st.markdown(message["content"])
+if "messages" not in st.session_state:
+    st.session_state.messages = []
 
-    # Accept user input
-    if prompt := st.chat_input("What is up?"):
-        # Add user message to chat history
-        st.session_state.messages.append({"role": "user", "content": prompt})
-        # Display user message in chat message container
-        with st.chat_message("user"):
-            st.markdown(prompt)
+for message in st.session_state.messages:
+    with st.chat_message(message["role"]):
+        st.markdown(message["content"])
 
-        # Display assistant response in chat message container
-        with st.chat_message("assistant"):
-            message_placeholder = st.empty()
-            full_response = ""
-            # for the response what to do ?? ==> send the prompt and return the repsonse from our chatgpt
-            # ---------------------------------
-            # ACTUAL USE OF FUNCTION HERE
-            assistant_response = chatbot.create_response(prompt)
-            assistant_response = assistant_response['choices'][0]['message']['content']
+if prompt := st.chat_input("What is up?"):
+    st.session_state.messages.append({"role": "user", "content": prompt})
+    with st.chat_message("user"):
+        st.markdown(prompt)
 
-
-            # ---------------------------------
-            # assistant_response = chatbot.generate_response(prompt)
-            # Simulate stream of response with milliseconds delay
-
-            message_placeholder.markdown(assistant_response)
-        # Add assistant response to chat history
-        st.session_state.messages.append({"role": "assistant", "content": assistant_response})
-
-    # Add histogram data
-    x1 = np.random.randn(200) - 2
-    x2 = np.random.randn(200)
-    x3 = np.random.randn(200) + 2
-
-    # Group data together
-    hist_data = [x1, x2, x3]
-    group_labels = ['Group 1', 'Group 2', 'Group 3']
-
-    # Create distplot with custom bin_size
-    fig = ff.create_distplot(hist_data, group_labels, bin_size=[.1, .25, .5])
-
-    # Create a 3-column layout
-    col1, col2, col3 = st.columns(3)
-
-    # Use the middle column for the plot
-    col2.plotly_chart(fig, use_container_width=True)
-
-# Run the Streamlit app
-if __name__ == "__main__":
-    main()
+    with st.chat_message("assistant"):
+        message_placeholder = st.empty()
+        full_response = ""
+        for response in openai.ChatCompletion.create(
+            engine=model_engine,
+            temperature=temperature,
+            messages=[
+                {"role": m["role"], "content": m["content"]}
+                for m in st.session_state.messages
+            ],
+            stream=True,
+        ):
+            full_response += response.choices[0].delta.get("content", "")
+            message_placeholder.markdown(full_response + "▌")
+        message_placeholder.markdown(full_response)
+    st.session_state.messages.append({"role": "assistant", "content": full_response})
